ce87cc10cded08e1a56d15257272687c4d4bacb2
shwwu <limitss2010@gmail.com>
2020-07-05 23:29:54 +0800

    hank 測式git

---
+++ tab.py
@@ -0,0 +1,26 @@
+"""
+this is a test
+
+"""
+#python Tab
+import sys
+
+import rlcompleter
+import atexit
+import os
+try:
+  import readline
+except ImportError:
+  import pyreadline as readline
+
+readline.parse_and_bind('tab: complete')
+# windows
+histfile = os.path.join(os.environ['HOMEPATH'], '.pythonhistory')
+# linux
+# histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+atexit.register(readline.write_history_file, histfile)
+del os, histfile, readline, rlcompleter
